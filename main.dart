d36a45e65f46a1892fe6c8258ddaf7b11651a0e3
Young727TW <62501690+Young727TW@users.noreply.github.com>
2021-04-25 14:09:19 +0800

    start to learn dart&flutter

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+import 'package:flutter/material.dart';
+// void main(){
+//   return runApp(MaterialApp());
+// }
+
+void main() => runApp(MaterialApp(
+      home:new HomePage(),
+    ));
+
+
+class HomePage extends StatelessWidget{
+  
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
